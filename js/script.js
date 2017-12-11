63e8627cff9819effc64b0a185f0c2e838593282
Evaldas Stankus <evalsta@gmail.com>
2017-12-11 19:49:53 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var auto = [
+    ['ZBG 123', 98450, 3645],
+    ['ABC 345', 1500, 91],
+    ['LRS 222', 49506, 3250],
+    ['LRS 222',  985622,  7551]
+];
+
+for (var i=0; i< auto.length; i++){
+    console.log("automobilio valstybiniai numeriai:" + auto[i][0] + "nuvaziuotas atstumas:" + auto[i][1] + "Sugaistas laikas:" + auto [i][2]);
+    console.log (auto[i]);
+}
