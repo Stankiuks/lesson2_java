80803c484e18931396a7489fa1f836e86bf84062
Evaldas Stankus <evalsta@gmail.com>
2017-12-12 18:42:08 +0200

    suvestas kodas javos

--- js/script.js
+++ js/script.js
@@ -5,7 +5,45 @@ var auto = [
     ['LRS 222',  985622,  7551]
 ];
 
-for (var i=0; i< auto.length; i++){
-    console.log("automobilio valstybiniai numeriai:" + auto[i][0] + "nuvaziuotas atstumas:" + auto[i][1] + "Sugaistas laikas:" + auto [i][2]);
-    console.log (auto[i]);
+var headerAuto = ['Valstybiniai numeriai', 'Nuvaziuotas atstumas, km', 'Sugaistas laikas, h', 'Vidutinis greitis, km/h'];
+
+//Kvieciame funkcija kuri atspausdins lentele
+createTable(auto, headerAuto);
+
+//Lenteles sukurimas
+function createTable(auto, headerAuto){
+    var text = '';
+
+    text += '<table border="2">';
+        text += '<thead>';
+            text += '<tr>';
+                for(var i =0; i<headerAuto.length; i++){
+                    text += '<td>' + headerAuto[i] + '</td>';
+
+                }
+            text += '</tr>';
+        text += '</thead>';
+        text += '<tbody>';
+            for(var i=0; i < auto.length; i++){
+                text += '<tr>';
+                    var greitis, laikas;
+                    for (var j = 0; j < auto[i].length; j++){
+                        text += '<td>' + auto [i][j] + '</td>';
+                        switch (j){
+                            case 1:
+                                atstumas = auto [i][j]/1000;
+                                break;
+                            case 2:
+                                laikas = auto [i][j]/3600;
+                                break;
+                        }
+                    }
+                    text += '<td>' + (atstumas / laikas).toFixed(0) + '</td>';
+                text += '</tr>'
+
+            }
+        text += '</tbody>';
+    text += '</table>';
+
+    document.querySelector('.table').innerHTML = text;
 }
